0fac0d0231c8a9354e44cd9685ad7896862b6654
Mèo Coder <truongtaman2004@gmail.com>
2025-01-29 22:57:35 +0700

    Update Movement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] float _moveSpeed = 2.0f;
-    void Start()
-    {
-        
+    [SerializeField] private float _moveSpeed = 2.0f;
+    private Rigidbody2D _rb;
+    private PlayerControls _playerControls;
+
+    private Vector2 _movementVector;
+
+    private void Awake() {
+        _rb = this.GetComponent<Rigidbody2D>();
+        _playerControls = new PlayerControls();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable() {
+        _playerControls.Enable();
+    }
+
+    private void Update() {
+        GetInput();
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void GetInput() {
+        _movementVector = _playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move() {
+        _rb.MovePosition(_rb.position + _movementVector * _moveSpeed * Time.fixedDeltaTime);
     }
 }
