f1ef62e3f2ce0e7300f3dfbcdac6ff4372bb983d
Mèo Coder <truongtaman2004@gmail.com>
2025-01-29 23:35:24 +0700

    Add Idle and Running animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float _moveSpeed = 2.0f;
     private Rigidbody2D _rb;
     private PlayerControls _playerControls;
+    private Animator _anim;
+    private Transform _trans;
+    private SpriteRenderer _spRen;
 
     private Vector2 _movementVector;
 
     private void Awake() {
         _rb = this.GetComponent<Rigidbody2D>();
         _playerControls = new PlayerControls();
+        _anim = this.GetComponent<Animator>();
+        _trans = this.GetComponent<Transform>();
+        _spRen = this.GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable() {
@@ -24,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
@@ -32,6 +38,17 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Move() {
+        if (_movementVector.magnitude > .0f) 
+            _anim.SetBool("isRunning",true); 
+        else 
+            _anim.SetBool("isRunning",false);
+
         _rb.MovePosition(_rb.position + _movementVector * _moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void AdjustPlayerFacingDirection() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (mousePos.x < _trans.position.x) _spRen.flipX = true;
+        else _spRen.flipX = false;
     }
 }
